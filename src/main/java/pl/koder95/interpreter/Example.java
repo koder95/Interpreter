429856a9aa85fb0905d55d397d094c3fa7eaef2f
Kamil Jan Mularski <kammul@outlook.com>
2025-02-15 00:05:18 +0100

    Create Example.java

--- src/main/java/pl/koder95/interpreter/Example.java
+++ src/main/java/pl/koder95/interpreter/Example.java
@@ -1,34 +1,3 @@
-# Wzorzec projektowy Interpreter
-Projekt jest implementacją wzorca projektowego *Interpreter* w wersji uniwersalnej.
-
-Interfejs [klienta](https://github.com/koder95/Interpreter/blob/master/src/main/java/pl/koder95/interpreter/Client.java) pozwala na stworzenie nowej instancji
-[interpretera](https://github.com/koder95/Interpreter/blob/master/src/main/java/pl/koder95/interpreter/Interpreter.java), który będzie działał w podanym
-[kontekście](https://github.com/koder95/Interpreter/blob/master/src/main/java/pl/koder95/interpreter/Context.java).
-
-Języki formalne można podzielić na dwie grupy ze względu na kontekst: języki formalne bezkontekstowe
-i języki formalne kontekstowe. Kontekst określa ostateczne znaczenie
-[wyrażeń](https://github.com/koder95/Interpreter/blob/master/src/main/java/pl/koder95/interpreter/Expression.java)
-wieloznacznych w perspektywie tylko jednego wyrażenia. Jeśli każde z wyrażeń jest jednoznaczne i nie potrzebuje
-uściśleń przez kontekst, wówczas należy używać instancji utworzonej przez
-`new pl.koder95.interpreter.Context(){}`, co oznacza gramatykę bezkontekstową.
-
-Interfejs [Interpreter](https://github.com/koder95/Interpreter/blob/master/src/main/java/pl/koder95/interpreter/Interpreter.java) dostarcza nie tylko
-[metodę interpretującą odczytywane wartości](https://github.com/koder95/Interpreter/blob/b3f9b37f580d7580785ac4515601e07ce9b38d85/src/main/java/pl/koder95/interpreter/Interpreter.java#L38),
-ale również implementacje interfejsu [parsera](https://github.com/koder95/Interpreter/blob/master/src/main/java/pl/koder95/interpreter/Parser.java),
-który wykorzystywany jest do budowania drzewa abstrakcyjnej syntaktyki za pomocą metody
-[buildAbstractSyntaxTree(java.util.Queue)](https://github.com/koder95/Interpreter/blob/2d2197598c8597dac693b7520f52d9bac62c9ade/src/main/java/pl/koder95/interpreter/Parser.java#L23).
-
-## Zależność Maven
-```xml
-<dependency>
-    <groupId>pl.koder95</groupId>
-    <artifactId>Interpreter</artifactId>
-    <version>1.0-SNAPSHOT</version>
-</dependency>
-```
-
-## Przykład
-```java
 package pl.koder95.interpreter;
 
 import java.io.StringReader;
@@ -264,4 +233,3 @@ public final class Example {
         }
     }
 }
-```
